f5c482bf13b703c8e6f1f7b8dacf8f0d140f6b51
Garry <avgk.udey@gmail.com>
2020-08-05 10:59:10 +0530

    Initial commit

---
+++ uicomponent/src/main/java/com/teracode/uicomponent/LiquidSwitch.java
@@ -0,0 +1,29 @@
+package com.teracode.uicomponent;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.LayoutInflater;
+import android.widget.LinearLayout;
+
+import androidx.annotation.Nullable;
+
+import com.airbnb.lottie.LottieAnimationView;
+
+public class LiquidSwitch extends LinearLayout {
+    private LottieAnimationView liquid_switch;
+
+
+    public LiquidSwitch(Context context) {
+        super(context);
+    }
+
+    public LiquidSwitch(Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+
+    private void init() {
+        LayoutInflater.from(getContext()).inflate(R.layout.liquid_switch, this);
+        liquid_switch = findViewById(R.id.liquid_switch);
+    }
+}
